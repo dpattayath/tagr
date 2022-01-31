c916f34b74fc32ef98fbb78ca5306f8a2fe2ff9c
Dileep Pattayath <dileep@theright.fit>
2022-01-31 22:22:06 +1000

    feat: integrating barcode scanner

--- resources/js/components/Merchant.js
+++ resources/js/components/Merchant.js
@@ -20,7 +20,7 @@ const Merchant = () => {
         error: ''
     });
     const [price, setPrice] = useState({
-        value: 0.0,
+        value: '',
         error: ''
     });
     const [sku, setSku] = useState({
@@ -38,7 +38,7 @@ const Merchant = () => {
             setDescription({'value': '', 'error': 'Description is required'});
             valid = false;
         }
-        if (price.value === 0) {
+        if (!price.value) {
             setPrice({'value': '', 'error': 'Price is required'});
             valid = false;
         }
@@ -59,9 +59,9 @@ const Merchant = () => {
             headers: { 'Content-Type': 'application/json', Accept: 'application/json'},
             body: JSON.stringify({
                 'title': title.value,
-                'description': description,
-                'price': price,
-                'sku': sku
+                'description': description.value,
+                'price': price.value,
+                'sku': sku.value
             })
         };
         fetch("api/merchants/products", requestOptions)
@@ -69,10 +69,10 @@ const Merchant = () => {
             .then(
                 (result) => {
                     setSpinner(false);
-                    setTitle('');
-                    setDescription('');
-                    setPrice('');
-                    setSku('');
+                    setTitle({'value': '', 'error': ''});
+                    setDescription({'value': '', 'error': ''});
+                    setPrice({'value': '', 'error': ''});
+                    setSku({'value': '', 'error': ''});
                 },
                 (error) => {
                     setSpinner(false);
@@ -81,98 +81,96 @@ const Merchant = () => {
     }
     return (
         <div className="container merchant">
-            <div className="main">
-                    { showForm ?
-                        <Card>
-                            <CardBody className="p-5">
-                                {
-                                    spinner ?
-                                        (
-                                            <div>
-                                                <Spinner type="grow" color="primary" size="sm"/>
-                                                <Spinner type="grow" color="primary" size="sm"/>
-                                                <Spinner type="grow" color="primary" size="sm"/>
+            { showForm ?
+                <Card>
+                    <CardBody className="p-5">
+                        {
+                            spinner ?
+                                (
+                                    <div>
+                                        <Spinner type="grow" color="primary" size="sm"/>
+                                        <Spinner type="grow" color="primary" size="sm"/>
+                                        <Spinner type="grow" color="primary" size="sm"/>
+                                    </div>
+                                )
+                                : (
+                                    <div>
+                                        <div className="form-group">
+                                            <div className="drag-drop">
+                                                Drag and drop, or browse to choose an image
                                             </div>
-                                        )
-                                        : (
-                                            <div>
-                                                <div className="form-group">
-                                                    <div className="drag-drop">
-                                                        Drag and drop, or browse to choose an image
-                                                    </div>
-                                                </div>
-                                                <div className="form-group">
-                                                    <label>Title</label>
-                                                    <input type="text"
-                                                           name="title"
-                                                           id="title"
-                                                           value={title.value}
-                                                           className="form-control"
-                                                           placeholder="E.g. Riders T-Shirt"
-                                                           onChange={(event) => setTitle({'value': event.target.value})}/>
-                                                    <span className="text-danger">{title.error}</span>
-                                                </div>
-                                                <div className="form-group">
-                                                    <label>Description</label>
-                                                    <textarea
-                                                        name="description"
-                                                        id="description"
-                                                        value={description.value}
-                                                        className="form-control"
-                                                        placeholder="Write a description"
-                                                        rows="4"
-                                                        onChange={(event) => setDescription({'value': event.target.value})}>
-                                                </textarea>
-                                                    <span className="text-danger">{description.error}</span>
-                                                </div>
-                                                <div className="form-group">
-                                                    <label>Price</label>
-                                                    <div className="input-group">
-                                                        <span className="currency">$</span>
-                                                        <input type="number"
-                                                               name="price"
-                                                               id="price"
-                                                               value={price.value}
-                                                               className="form-control border-left-0"
-                                                               placeholder="Enter an amount"
-                                                               onChange={(event) => setPrice({'value': event.target.value})}/>
-                                                    </div>
-                                                    <span className="text-danger">{price.error}</span>
-                                                </div>
-                                                <div className="form-group">
-                                                    <label>SKU Number</label>
-                                                    <input type="text"
-                                                           name="sku"
-                                                           id="sku"
-                                                           value={sku.value}
-                                                           className="form-control"
-                                                           placeholder="Enter SKU/barcode number"
-                                                           onChange={(event) => setSku({'value': event.target.value})}/>
-                                                    <span className="text-danger">{sku.error}</span>
-                                                </div>
-                                                <div className="d-flex justify-content-between mt-4">
-                                                    <Button className="btn-default"
-                                                            color="light"
-                                                            onClick={() => setShowForm(false)}>Cancel</Button>
-                                                    <Button className="btn-default"
-                                                            color="primary"
-                                                            onClick={addProduct}>Save</Button>
-                                                </div>
+                                        </div>
+                                        <div className="form-group">
+                                            <label>Title</label>
+                                            <input type="text"
+                                                   name="title"
+                                                   id="title"
+                                                   value={title.value}
+                                                   className="form-control"
+                                                   placeholder="E.g. Riders T-Shirt"
+                                                   onChange={(event) => setTitle({'value': event.target.value})}/>
+                                            <span className="text-danger">{title.error}</span>
+                                        </div>
+                                        <div className="form-group">
+                                            <label>Description</label>
+                                            <textarea
+                                                name="description"
+                                                id="description"
+                                                value={description.value}
+                                                className="form-control"
+                                                placeholder="Write a description"
+                                                rows="4"
+                                                onChange={(event) => setDescription({'value': event.target.value})}>
+                                        </textarea>
+                                            <span className="text-danger">{description.error}</span>
+                                        </div>
+                                        <div className="form-group">
+                                            <label>Price</label>
+                                            <div className="input-group">
+                                                <span className="currency">$</span>
+                                                <input type="number"
+                                                       name="price"
+                                                       id="price"
+                                                       value={price.value}
+                                                       className="form-control border-left-0"
+                                                       placeholder="Enter an amount"
+                                                       onChange={(event) => setPrice({'value': event.target.value})}/>
                                             </div>
-                                        )
-                                }
-                            </CardBody>
-                        </Card>
-                        :
-                        <div>
-                            <input type="button"
-                                   className="btn-add"
-                                   value="+"
-                                   onClick={() => setShowForm(true)}/>
-                            <span className="lbl-add">Add a new Product</span>
-                        </div>
-                    }
+                                            <span className="text-danger">{price.error}</span>
+                                        </div>
+                                        <div className="form-group">
+                                            <label>SKU Number</label>
+                                            <input type="text"
+                                                   name="sku"
+                                                   id="sku"
+                                                   value={sku.value}
+                                                   className="form-control"
+                                                   placeholder="Enter SKU/barcode number"
+                                                   onChange={(event) => setSku({'value': event.target.value})}/>
+                                            <span className="text-danger">{sku.error}</span>
+                                        </div>
+                                        <div className="d-flex justify-content-between mt-4">
+                                            <Button className="btn-default"
+                                                    color="light"
+                                                    onClick={() => setShowForm(false)}>Cancel</Button>
+                                            <Button className="btn-default"
+                                                    color="primary"
+                                                    onClick={addProduct}>Save</Button>
+                                        </div>
+                                    </div>
+                                )
+                        }
+                    </CardBody>
+                </Card>
+                :
+                <div>
+                    <input type="button"
+                           className="btn-add"
+                           value="+"
+                           onClick={() => setShowForm(true)}/>
+                    <span className="lbl-add">Add a new Product</span>
                 </div>
+            }
         </div>
     );
 }
--- resources/js/components/Shopper.js
+++ resources/js/components/Shopper.js
@@ -1,23 +1,49 @@
 
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import {
-    Button
-} from "reactstrap";
+import {Button} from "reactstrap";
 import {BrowserView, MobileView} from 'react-device-detect';
+import ScanditBarcodeScanner from "scandit-sdk-react";
+import {Barcode, ScanSettings} from "scandit-sdk";
 
 const Shopper = () => {
+
+    const [scannerReady, setScannerReady] = useState(false);
+    const licenseKey = 'AVvBHD2fHFYQAdCRNiFd3gwoDDvyJ7LjTlu7occLik75cFQvDkjAtJ1DkmnwadICJXgrk9N5pKsWbisOzVcpqhZ9wSo+RcS5mH/0n1dmm9IjN6KmvlreSDVMmYyBcX0WHmMCNHRspR2gEfC4gQ6BecsyGnFfIc/bcRFnXxbky3oDBe3iQRrX4qdXV202a4fphciX5oEJTwrVSsczlisR9RQ5aoAA9vQWaJtPLGUqp9lXf2mNZJLanoghuwmRvtyhTvxIK1EjN6YaQdAwqgJAmv14iBnkyeKfZdY7jsGxfvRsOpmuT5dsxui1NEe2EL+Xp71E2k0kLw4xg5cwjqaohhlnjz6TwLxjIQlxyvHHs+1XBWk4BWD+luc7l6brCVduaMqru0/SEBTAL4sdjYHOc21laveGxeGW2Cw2aMVVU76GNb5jGlC+m/oJ550XkrMCXpVHV7Q2pLkLAZ4+NoIPu4bbcDF+C8B+FBkkI/192gdZwqPU91rr325RmFmxgoqEugbtR7uXpxXOa8wwKGCTssNTK2b9a+LSaSuSys3sUPJUbBjGDqgA+pp2lWVm1NQjAi46SNu+VtRUclU7mYjC/RggTZc2tg0qsE7GAORWSPDwu+eT0eSW9RdsCAW1IVMeIuA4RxbxxHSAZNUDanLfLTl3xiQhNm8YBxIs0G5ZXoQlGRp4u4mN6Ev2Vm5SK74NsujdozSXirvoPrHTgqK6oaPhgwFvQ8Kv0hfFVWm51iAI16+J/DrLnHQ98aEwJWmRT+HKQ2jpZa37qKgfb4tnbbzlavjbUlu0LQ97phOnxs7g3dTI57arikjAHk2+mNKE4rr8ACaj71Q=';
+    const getScanSettings = () => {
+        return new ScanSettings({
+            enabledSymbologies: [Barcode.Symbology.CODE128],
+            viewfinderArea: { x: 0, y: 0, width: 1, height: 1 }
+        });
+    };
+
     return (
         <div className="container shopper">
             <MobileView>
-                <div className="compatible">
-                    <div className="title">
-                        Mobile Self-Checkout? <br/>Try it now.
+                {scannerReady ?
+                    <div className="scanner">
+                        <ScanditBarcodeScanner
+                            licenseKey={licenseKey}
+                            engineLocation="https://cdn.jsdelivr.net/npm/scandit-sdk@5.x/build"
+                            onScan={console.log}
+                            onScanError={console.log}
+                            scanSettings={getScanSettings()}
+                        />
+                        <div className="actions">
+                            <Button className="btn-cancel"
+                                    onClick={() => setScannerReady(false)}>Cancel</Button>
+                        </div>
                     </div>
-                    <div className="actions">
-                        <Button className="btn-scan">Scan barcode</Button>
+                    : <div className="compatible">
+                        <div className="title">
+                            Mobile Self-Checkout? <br/>Try it now.
+                        </div>
+                        <div className="actions">
+                            <Button className="btn-scan"
+                                    onClick={() => setScannerReady(true)}>Scan barcode</Button>
+                        </div>
                     </div>
-                </div>
+                }
             </MobileView>
             <BrowserView>
                 <div className="incompatible">
